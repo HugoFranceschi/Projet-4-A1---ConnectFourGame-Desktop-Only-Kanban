e2dbd6c66b8739624de4e2e7312517e900cca3a1
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-02-19 12:01:46 +0100

    liste 16

--- Projet 4 A1 - ConnectFourGame Desktop Only Kanban/app.js
+++ Projet 4 A1 - ConnectFourGame Desktop Only Kanban/app.js
@@ -1,3 +1,6 @@
 const dialogRegle = document.querySelector("dialog.regle");
+const dialogPause = document.querySelector("dialog.pause");
+const dialogAcceuil = document.querySelector("dialog.accueil");
 
 dialogRegle.showModal();
+// dialogPause.showModal();

27b4d0cc9fde6561e2814ffc229e4bcf5c993af8
HugoFranceschi <franceschi.hugo@yahoo.com>
2025-02-24 17:29:52 +0100

    24

--- Projet 4 A1 - ConnectFourGame Desktop Only Kanban/app.js
+++ Projet 4 A1 - ConnectFourGame Desktop Only Kanban/app.js
@@ -126,9 +126,66 @@ const grille = [
 	["", "", "", "", "", "", ""],
 	["", "", "", "", "", "", ""],
 ];
-
 var Score1 = 0;
 var Score2 = 0;
 var colonne = 0;
 var NonDuJoueur = "";
 var chronomètre = 30;
+
+const grilleAvecGagnant1 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["X", "X", "X", "X", "", "", ""],
+];
+
+const grilleAvecGagnant2 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+];
+
+const grilleAvecGagnant3 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "X", "", "", ""],
+	["", "", "X", "", "", "", ""],
+	["", "X", "", "", "", "", ""],
+	["X", "", "", "", "", "", ""],
+];
+
+const grilleAvecGagnant4 = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "X", "", "", ""],
+	["", "", "X", "O", "", "", ""],
+	["", "O", "O", "O", "", "", ""],
+	["X", "O", "O", "O", "O", "", ""],
+];
+
+const grilleSansGagnant = [
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "X", "", "", ""],
+	["", "", "X", "O", "", "", ""],
+	["", "O", "O", "O", "", "", ""],
+	["X", "O", "O", "X", "O", "", ""],
+];
+
+function checkWinner(grille) {
+	let tableau = grille[5][0];
+	console.log(tableau);
+}
+
+let resultat = "";
+
+resultat = checkWinner(grilleAvecGagnant1); // retourne "X"
+resultat = checkWinner(grilleAvecGagnant2); // retourne "X"
+resultat = checkWinner(grilleAvecGagnant3); // retourne "X"
+resultat = checkWinner(grilleAvecGagnant4); // retourne "O"
+resultat = checkWinner(grilleSansGagnant); // retourne ""
